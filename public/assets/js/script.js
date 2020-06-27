93325f3cde23f42ee4b4ade2e29272423869a941
John Bouchard <jbouchard13@gmail.com>
2020-06-27 00:17:22 -0400

    added front end ajax request for adding data to db

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,4 +5,22 @@ $(".devour-btn").on("click", (e) => {
   alert("clicked");
 });
 
-$(".submit-btn").on("click", () => {});
+$(".submit-btn").on("click", (e) => {
+  // prevent the page from defaulting so the data stays through the put request
+  e.preventDefault();
+  // get the data value from the text field
+  const newBurger = {
+    // set the burger_name to the data entered
+    burger_name: $("#addBurger").val(),
+  };
+  // call an ajax put request to /api/burgers
+  $.ajax("/api/burgers", {
+    // send the new burger data to the back end to be added to the db
+    type: "POST",
+    data: newBurger,
+  }).then(() => {
+    console.log(newBurger);
+    // reload the page to display the added burger to the user
+    location.reload();
+  });
+});
